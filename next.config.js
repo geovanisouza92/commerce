a1157d6cab9948e754b6283aa53ea1818308d81e
Geovani de Souza <geovanisouza92@gmail.com>
2021-12-23 12:53:28 -0300

    Dynamic locale configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
+const { readdirSync, readFileSync } = require('fs')
 const commerce = require('./commerce.config.json')
 const { withCommerceConfig } = require('./framework/commerce/config')
 
+function getI18n() {
+  let defaultLocale
+  const locales = []
+
+  readdirSync('./public/locale', { withFileTypes: true }).forEach(dirent => {
+    if (dirent.name === '.defaultLocale') {
+      defaultLocale = readFileSync(`./public/locale/.defaultLocale`, 'utf8').trim()
+      return
+    }
+    locales.push(dirent.name)
+  })
+
+  return { locales, defaultLocale }
+}
+
 module.exports = withCommerceConfig({
   commerce,
-  i18n: {
-    locales: ['en-US', 'es'],
-    defaultLocale: 'en-US',
-  },
+  i18n: getI18n(),
 })
 
 // Don't delete this console log, useful to see the commerce config in Vercel deployments
